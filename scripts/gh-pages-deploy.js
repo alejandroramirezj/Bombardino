b85b38de6ac3ead753d6ab69c5d37d3999eaf861
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2025-04-13 15:56:08 +0200

    Mejorar script de despliegue para asegurar archivo CNAME

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 // Copiar index.html a 404.html para el manejo de rutas en GitHub Pages
 const indexHtml = path.resolve(__dirname, '../dist/index.html');
 const errorHtml = path.resolve(__dirname, '../dist/404.html');
+const cnameFile = path.resolve(__dirname, '../dist/CNAME');
 
 // Asegurarse de que el archivo existe
 if (fs.existsSync(indexHtml)) {
@@ -17,6 +18,12 @@ if (fs.existsSync(indexHtml)) {
 } else {
   console.error('❌ No se encontró el archivo index.html en la carpeta dist');
   process.exit(1);
+}
+
+// Asegurarse de que existe el archivo CNAME para dominio personalizado
+if (process.env.CUSTOM_DOMAIN === 'true') {
+  fs.writeFileSync(cnameFile, 'bombardirocrocodrilo.com');
+  console.log('✅ Se ha creado/verificado el archivo CNAME para el dominio personalizado');
 }
 
 console.log('📦 La aplicación está lista para ser desplegada en GitHub Pages'); 
