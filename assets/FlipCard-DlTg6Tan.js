56da5a7f41b38f42d6f3aa1ae778033459cf08c0
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2025-04-25 16:02:47 +0200

    Updates

--- assets/FlipCard-DlTg6Tan.js
+++ assets/FlipCard-DlTg6Tan.js
@@ -1,4 +1,4 @@
-import{c,r as o,j as e,m as t}from"./index-DKymhv2o.js";/**
+import{c,r as o,j as e,m as t}from"./index-CANpmtaV.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
