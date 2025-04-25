317d3641f064b6055572dc7ecd363f55bf81adb7
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2025-04-25 15:43:44 +0200

    Updates

--- assets/arrow-right-B7_QiUIj.js
+++ assets/arrow-right-B7_QiUIj.js
@@ -1,4 +1,4 @@
-import{c as r}from"./index-BEg6K-WN.js";/**
+import{c as r}from"./index-DKymhv2o.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
